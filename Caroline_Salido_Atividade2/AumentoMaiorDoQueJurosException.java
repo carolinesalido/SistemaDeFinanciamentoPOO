e4e26514a670dcaa60b63b083c77d9f313098e25
Caroline Salido <salidocarol@gmail.com>
2026-05-22 12:52:46 -0300

    Remove estrutura antiga e mantem organizacao em src/

--- Caroline_Salido_Atividade2/AumentoMaiorDoQueJurosException.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-public class AumentoMaiorDoQueJurosException extends RuntimeException {
-    public AumentoMaiorDoQueJurosException(String message) {
-        super(message);
-    }
-}
--- Caroline_Salido_Atividade2/metodos_adicionais_entrada.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public interface metodos_adicionais_entrada {
-    public double atributos_adicionais_casa_area_construida();
-    public double atributos_adicionais_casa_tamanho_terreno();
-    public int atributos_adicionais_apto_numero_vagas_garagem();
-    public int atributos_adicionais_apto_andar();
-    public boolean atributos_adicionais_terreno_tipo_uso();
-}
